ebbfa33fa29ade5a7f05cc52735b2aa2df25b22d
GitHub Action <action@github.com>
2025-03-26 13:28:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/kibettheophilus/buildlogicgeneratorideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/kibettheophilus/buildlogicgeneratorideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kibettheophilus.buildlogicgeneratorideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
